47b014d3b0d663d2ee208b4410e4ba3e6de7bbb7
SETKEH <setkeh@gmail.com>
2013-06-18 15:19:48 +1000

    First Commit

---
+++ cgminerstats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2.7
+
+from pga0 import *
+from cgminerversion import *
+from cgminerconfig import *
+from coin import *
+
+print "CGMiner:"
+print "Cgminer Version:", cg_version()
+print "Api Version:", api_version()
+print "PGA's Connected:", pga_count()
+print "ASIC's Connected:", asic_count()
+print "Pool Count:", pool_count()
+print "Pool Stratergy:", pool_stratergy()
+print "Miner OS:", miner_os()
+print ""
+
+print "Bitcoin Status:"
+print "Hash Method:", hash_method()
+print "Block Hash:", current_block_hash()
+print "Long Poll:", long_poll()
+print "Network Diff:", diff()
+print""
+
+print "PGA 0:"
+print p0_mhs(), "MH/s"
+print "Accepted Shares:", p0_accepted()
+print "Rejected Shares:", p0_rejected()
+print "Hardware Errors:", p0_hwerrors()
+print "Utility:", p0_utility()
---
+++ cgminerversion.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2.7
+
+import socket
+import json
+
+def linesplit(socket):
+	buffer = socket.recv(4096)
+	done = False
+	while not done:
+		more = socket.recv(4096)
+		if not more:
+			done = True
+		else:
+			buffer = buffer+more
+	if buffer:
+				return buffer
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(('192.168.1.6',4028))
+s.send(json.dumps({"command":"version","parameter":"CGMiner"}))    
+response = linesplit(s)
+response = response.replace('\x00','')
+response = json.loads(response)
+def cg_version():
+    return response['VERSION'][0]['CGMiner']
+def api_version(): 
+    return response['VERSION'][0]['API'] 
+s.close()
